caed1eace0a28ae80d0eaa3f7258f6d2a4c3d398
John DeBord <john.debord@block.one>
2019-12-07 19:27:02 -0500

    Update simple.el

--- lisp/simple.el
+++ lisp/simple.el
@@ -8843,9 +8843,14 @@ See also `normal-erase-is-backspace'."
 
     (cond ((display-symbol-keys-p)
 	   (let ((bindings
-		  '(([M-delete] [M-backspace])
-		    ([C-M-delete] [C-M-backspace])
-		    ([?\e C-delete] [?\e C-backspace]))))
+		  ;;; John DeBord
+		  ;;; Dec. 1st, 2019
+		  ;;; Commenting out due to recieving the error:
+                  ;;; `error: Key sequence ESC <C-delete> starts with
+                  ;;; non-prefix key ESC`
+		  ;; '(([M-delete] [M-backspace])
+		  ;;   ([C-M-delete] [C-M-backspace])
+		  ;;   ([?\e C-delete] [?\e C-backspace]))))
 
 	     (if enabled
 		 (progn
